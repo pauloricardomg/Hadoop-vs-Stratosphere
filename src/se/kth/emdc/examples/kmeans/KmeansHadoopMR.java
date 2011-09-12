a7c9d6f936e0aae07183cd9d87ba2fc0d2d1f603
Benedicto <gonadarush@gmail.com>
2011-09-12 16:58:37 +0200

    implemented the combiner

--- src/se/kth/emdc/examples/kmeans/KmeansHadoopMR.java
+++ src/se/kth/emdc/examples/kmeans/KmeansHadoopMR.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 
 public class KmeansHadoopMR {
@@ -47,6 +48,24 @@ public class KmeansHadoopMR {
 		
 	}
 	
+	public static class Combiner 
+	extends Reducer<Text,Text,Text,Text> {
+		private Point localCentroid = new Point(0,0);
+		public void reduce(Text key, Iterable<Text> points, Context context
+				) throws IOException, InterruptedException {
+			int x=0, y=0;
+			int length=0;
+			for (Text point : points) {
+				Point p = new Point(point.toString().split(" +"));
+				x += p.getX();
+				y += p.getY();
+				++length;
+			}
+			localCentroid.setX(x/length);
+			localCentroid.setY(y/length);
+			context.write(key, new Text(localCentroid.toString()));
+		}
+	}
 	
 	public static List<Point> getCenters() throws Exception{
 		
